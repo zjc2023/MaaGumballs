e236da81486ef81ec6d47027ca207507e42d384b
shanshanzi <3196497671@qq.com>
2025-05-18 14:47:03 +0800

    测试了一遍每日任务流程，对卡死的地方增加容错判断

--- agent/action/DailyTask.py
+++ agent/action/DailyTask.py
@@ -9,16 +9,11 @@ class DailyTask(CustomAction):
     def run(
         self, context: Context, argv: CustomAction.RunArg
     ) -> CustomAction.RunResult:
-        argv: dict = json.loads(argv.custom_action_param)
+        taskList: dict = json.loads(argv.custom_action_param)
+        print(taskList)
         
-        # 检查注释中的键值是否为1
-        task_keys = [
-            "DailySweep", "WildernessExplore", "SendLizards",
-            "AlchemySignboard", "DailySignIn", "SkyExplore", "RuinsExplore"
-        ]
-        
-        for key in task_keys:
-            if argv.get(key) == 1:
+        for key in taskList:
+            if taskList.get(key) == 1:
                 # 生成任务执行逻辑框架
                 print(f"执行任务: {key}")
                 context.run_task(key)
