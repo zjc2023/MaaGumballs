85567fc449a84e0d19681f0f658df63a537911ab
shanshanzi <3196497671@qq.com>
2025-05-30 12:19:05 +0800

    chore: 图片路径修复

--- agent/action/Fight/JJC101.py
+++ agent/action/Fight/JJC101.py
@@ -95,7 +95,7 @@ class JJC101_Select(CustomAction):
         recodetail = context.run_recognition("JJC_Select_Gumball_Check", img, pipeline_override={
             "JJC_Select_Gumball_Check": {
                 "recognition": "FeatureMatch",
-                "template": "monster/JJC/夜叉冈布奥.png",
+                "template": "fight/JJC/夜叉冈布奥.png",
                 "roi": [
                     400,
                     645,
@@ -110,7 +110,7 @@ class JJC101_Select(CustomAction):
             logger.info("夜叉未检测到, 自动选择中")
             context.run_task("JJC_Select_Gumball_2", pipeline_override={
                 "JJC_Select_Gumball_Next":{
-                "template": "monster/JJC/夜叉冈布奥.png",
+                "template": "fight/JJC/夜叉冈布奥.png",
                 }
             })
 
