396436da36d93af24579c575ae4b13b0c18787fb
Norton_qk <82144715+fictionalflaw@users.noreply.github.com>
2025-06-16 17:19:51 +0800

    perf: 增加custom_action_param中的run_node字段，用于增加泛用性 (#13)
    * feat:增加custom_action_param中的run_node字段，用于增加泛用性
    
    没找到使用了Count这个custom action的节点，直接改在action里了
    
    * feat:CountTask追加run_node字段处理
    
    * Update tl01_fight.json

--- agent/action/count.py
+++ agent/action/count.py
@@ -1,6 +1,8 @@
 from maa.agent.agent_server import AgentServer
 from maa.context import Context
 from maa.custom_action import CustomAction
+from utils import logger
+
 import json
 
 @AgentServer.custom_action("Count")
@@ -21,14 +23,15 @@ class Count(CustomAction):
                     },
                 }
             )
-            print(f"execute {argv.get('count')}  TL01_Start")
-            context.run_task("TL01_Start")
+            print(f"execute {argv.get('count')}  {argv.get("run_node"}")
+            context.run_task(argv.get("run_node"))
         else:
             context.override_pipeline(
                 {
                     argv.get("self"): {
                         "custom_action_param": {
                             "self": argv.get("self"),
+                            "run_node": "TL01_Start",
                             "count": 0,
                             "target_count": argv.get("target_count"),
                             "next_node": argv.get("next_node"),
@@ -47,17 +50,32 @@ class CountTask(CustomAction):
     def run(
         self, context: Context, argv: CustomAction.RunArg
     ) -> CustomAction.RunResult:
+        '''
+                自定义动作：
+        custom_action_param:
+            {
+                "count": int,
+                "target_count": int,
+                "next_node": string,
+                "run_node": string
+            }
+        count: 当前次数
+        target_count: 目标次数
+        next_node: 达到目标次数后执行的节点
+        run_node:未达到目标次数前执行的节点,可以是某个任务链的开始节点
+        '''
         argv: dict = json.loads(argv.custom_action_param)
-        print(argv)
+        logger.info(argv)
         if not argv:
             return CustomAction.RunResult(success=True)
 
         count = argv.get("count")
         target_count = argv.get("target_count")
-        print(f"count = {count}, target_count = {target_count}")
+        run_node = argv.get("run_node")
+        logger.info(f"count = {count}, target_count = {target_count}")
         while count < target_count:
-            print(f"excute {count} TL01_Start")
-            context.run_task("TL01_Start")
+            logger.info(f"excute {count} {run_node}")
+            context.run_task(run_node)
             count += 1
 
         for i in argv.get("next_node"):
