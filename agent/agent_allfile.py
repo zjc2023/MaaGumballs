d25c498c105fca5ebae8222fa55e30aee909a4f4
KhazixW2 <3196497671@qq.com>
2025-07-01 02:14:02 +0800

    chore: 去掉部分用不到的代码

--- agent/agent_allfile.py
+++ agent/agent_allfile.py
@@ -1,7 +1,5 @@
 from action.fight import jjc101
 from action.fight import getKeyFromHole
-from action.fight import tl01_Fighting
-from action.fight import checkGrid
 
 from action import count
 from action import dailyTask
