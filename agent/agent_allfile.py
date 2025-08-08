1cb73df5b2a4cf42046f69c35b95cf7dc28b6412
山山子 <46012438+KhazixW2@users.noreply.github.com>
2025-08-08 15:38:06 +0800

    feat:  自动批量兑换密令小工具

--- agent/agent_allfile.py
+++ agent/agent_allfile.py
@@ -9,5 +9,7 @@ from action import dailyTask
 from action import reward
 from action import shopping
 from action import ppOverride
+from action import AutoCDK
+
 
 from action.divineForgeLand import someTrick
