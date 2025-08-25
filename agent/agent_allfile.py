385d4a698e5675c542a4c2cf7596564137b41e39
zjc2023 <3038195945@qq.com>
2025-08-25 22:25:35 +0800

    自动天空第一版

--- agent/agent_allfile.py
+++ agent/agent_allfile.py
@@ -9,5 +9,6 @@ from action import dailyTask
 from action import reward
 from action import shopping
 from action import ppOverride
+from action import sky
 
 from action.divineForgeLand import someTrick
